499cabc4fb663075160a0c2cd60f30ab8b5baad5
Laura Harper <lauraharper95@gmail.com>
2024-04-02 12:12:49 +0100

    changed fetchHighestRatedMovies function - was returning lowest rated

--- src/containers/CineMavericksContainer.js
+++ src/containers/CineMavericksContainer.js
@@ -7,22 +7,22 @@ const CineMavericksContainer = () => {
     const [highestRatedMovies, setHighestRatedMovies] = useState([]);
     
     const fetchMovies = async () => {
-        const response = await fetch("localhost:8080/movies");
+        const response = await fetch("http://localhost:8080/movies");
         const data = await response.json();
         setMovies(data);
     };
 
     const fetchMovieLists = async () => {
-        const response = await fetch("localhost:8080/movielists");
+        const response = await fetch("http://localhost:8080/movielists");
         const data = await response.json();
         setMovieLists(data);
     };
 
     const fetchHighestRatedMovies = async () => {
-        const response = await fetch("");
+        const response = await fetch("http://localhost:8080/movies");
         const data = await response.json();
         const sortedData = data.sort((movie1, movie2) => {
-            return movie1.average_rating - movie2.average_rating;
+            return movie2.averageRating - movie1.averageRating;
         })
         setHighestRatedMovies(sortedData.slice(0,5));
     };
