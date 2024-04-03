f7d50d95456a996046ae17582198a40a6777c313
Harun Walizadeh <harunwalizadeh@gmail.com>
2024-04-03 16:45:33 +0100

    added event handlers for filter function

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,11 +1,39 @@
+import React, { useState } from "react";
 import MovieList from "./MovieList";
 
 const Movies = ({movies}) => {
+
+    const [genreFilter, setGenreFilter] = useState("");
+    const [minDuration, setMinDuration] = useState(0);
+    const [minRating, setMinRating] = useState(0);
+
+   const handleGenreChange = (event) => {
+    setGenreFilter(event.target.value);
+   };
+
+   const handleDurationChange = (event) => {
+    setMinDuration(parseInt(event.target.value));
+   };
+
+   const handleRatingChange = (event) => {
+    setMinRating(parseInt(event.target.value));
+   };
+
+   
+
+
+
+
     return ( 
+    <>
+     
         <>
-            <MovieList listOfMovies = {movies} />
+        <h2>Movies</h2>
+        <MovieList listOfMovies={filteredMovies} />
         </>
-     );
+    </>
+);
+
 }
  
 export default Movies;
