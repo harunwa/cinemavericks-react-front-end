d8ceb0c2feb193232756890643a968e28046d2f3
Tadiwanashe Dzvoti <tadiwadzvoti@hotmail.com>
2024-04-03 11:13:40 +0100

    Added more information about the review to the page.

--- src/components/Review.jsx
+++ src/components/Review.jsx
@@ -1,9 +1,25 @@
 const Review = ( { review } ) => {
+
+    const starRating = (rating) => {
+        let stars = '';
+        for(let i = 1; i <= rating; i++){
+            stars += '⭐️';
+        }
+        if(Math.ceil(rating) !== rating){
+            stars += '1/2';
+        }
+        return stars;
+    };
+
+    const date = review.date.split(' ')[0];
+
     return (  
         <>
+            <h3>{review.title}</h3>
             <p>Review by <strong>{review.user.name}</strong></p>
             <p>{review.content}</p>
-            <p>Rating: <strong>{review.rating}</strong></p>
+            <p>Rating: <strong>{starRating(review.rating)}</strong></p>
+            <p>{date}</p>
         </>
     );
 }
