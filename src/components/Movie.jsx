3921b2050efa0ec5147e7b6471332a4ce7984ea7
Harun Walizadeh <harunwalizadeh@gmail.com>
2024-04-03 10:14:28 +0100

    added movie page

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,18 +1,41 @@
-import {useLoaderData} from "react-router-dom";
+import React, { useEffect, useState } from 'react';
+import { useLoaderData, useParams } from 'react-router-dom';
 
 const Movie = () => {
+  const { id } = useParams();
+  const movie = useLoaderData();
 
-    const movie = useLoaderData();
+  useEffect(() => {
+    const fetchMovie = async () => {
+      try {
+        const response = await fetch(`http://localhost:8080/movies/${id}`);
+        if (!response.ok) {
+          throw new Error('Failed to fetch movie details');
+        }
+        const data = await response.json();
+      } catch (error) {
+        console.error('Error fetching movie details:', error);
+      }
+    };
 
-    const strippedMoveTitle = movie.title.replaceAll(' ', '');
-    
-    return ( 
+    fetchMovie();
+  }, [id]);
 
-        <>
-        
-        </>
+  return (
+    <div>
+      {movie ? (
+        <div>
+          <h2>{movie.title}</h2>
+          <p>Duration: {movie.duration} minutes</p>
+          <p>Director: {movie.director}</p>
+          <p>Genre: {movie.genre}</p>
+          <p>Release Year: {movie.year}</p>
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
+};
 
-     );
-}
- 
 export default Movie;
