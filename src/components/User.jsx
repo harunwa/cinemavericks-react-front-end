0263e595459713d64652009cff5530ebc3dab7a0
Harun Walizadeh <harunwalizadeh@gmail.com>
2024-04-04 12:12:54 +0100

    added user.css for login page

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,11 +1,14 @@
 import { Link } from "react-router-dom";
+import '../css/User.css' 
 
 const User = ( { user } ) => {
     return (  
-        <>
-            <p>{user.name}</p>
-            <button><Link to={`/user/${user.id}/cinema`}>Sign In!</Link></button>
-        </>
+        <div className="user">
+            <p className="user-name">{user.name}</p>
+            <button className="sign-in-button">
+            <Link to={`/user/${user.id}/cinema`}>Sign In!</Link>
+            </button>
+        </div>
     );
 }
  
