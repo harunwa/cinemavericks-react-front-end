5670aa1173039d9d6263478ef96c3752059a0db0
Tadiwanashe Dzvoti <tadiwapp365@hotmail.com>
2024-04-04 10:26:34 +0100

    Got rid of warning

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { useLoaderData, useParams } from 'react-router-dom';
 import Button from '@mui/material/Button';
 import Dialog from '@mui/material/Dialog';
