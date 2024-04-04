e846ce0f00120eaa0a85b14ea6b4f060f5738261
Tadiwanashe Dzvoti <tadiwapp365@hotmail.com>
2024-04-04 15:19:18 +0100

    Added title to movie lists pages.

--- src/components/MovieListList.jsx
+++ src/components/MovieListList.jsx
@@ -14,7 +14,6 @@ const MovieListList = ( { movieLists } ) => {
                 return <li key={movieList.id}><MovieList listOfMovies={movieList.movies} title={`${movieList.title} by ${movieList.user.name}`} userId={userId}/></li>;
             });
         }
-
         const filteredMovieLists = movieLists.filter(movieList => parseInt(userId) === movieList.user.id);
         return filteredMovieLists.map(movieList => {
             return <li key={movieList.id}><MovieList listOfMovies={movieList.movies} title={movieList.title} userId={userId}/></li>;
@@ -23,13 +22,17 @@ const MovieListList = ( { movieLists } ) => {
 
     return (  
         <>
-        <div id="graphic2">
-            <img id="landing-pic" src={landing} alt="An axolotl at the cinema with friends"/>
-        </div>
+            <h2>{userId === undefined || location.pathname.includes("public") ?
+             "All Movie Lists" :
+              "My Movie Lists"}
+            </h2>
+            <div id="graphic2">
+                <img id="landing-pic" src={landing} alt="An axolotl at the cinema with friends"/>
+            </div>
             <ul>
                 {loadComponents()}
             </ul>
-            </>
+        </>
     );
 }
  
